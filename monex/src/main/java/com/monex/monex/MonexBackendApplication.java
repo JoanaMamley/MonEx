21fbd11972473bc593f9a0f7063f5d2386512de8
Joana Mamley Teye <joanateye18@gmail.com>
2025-06-19 09:43:45 +0000

    backend starter with dependencies

---
+++ monex/src/main/java/com/monex/monex/MonexBackendApplication.java
@@ -0,0 +1,13 @@
+package com.monex.monex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonexBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonexBackendApplication.class, args);
+	}
+
+}
---
+++ monex/src/test/java/com/monex/monex/MonexBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monex.monex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonexBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
