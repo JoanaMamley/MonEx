6f9678cc1e6dbd95a355f89b1b5a6b51b9cfc6b4
Joana Mamley Teye <joanateye18@gmail.com>
2025-06-19 10:14:23 +0000

    backend starter with dependencies

---
+++ monex-backend/src/test/java/com/monex/monex_backend/MonexBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monex.monex_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonexBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
