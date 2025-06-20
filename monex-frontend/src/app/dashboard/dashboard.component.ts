22417139c381f66f0adb8e451513bf1ea6c76efe
Joana Mamley Teye <joanateye18@gmail.com>
2025-06-20 06:58:41 +0000

    create dashboard page with counter result displayed

--- monex-frontend/src/app/dashboard/dashboard.component.ts
+++ monex-frontend/src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { ConverterComponent } from './converter/converter.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, InputTextModule, ConverterComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+  formGroup: FormGroup | undefined;
 
+    ngOnInit() {
+        this.formGroup = new FormGroup({
+            text: new FormControl<string | null>(null)
+        });
+    }
 }
