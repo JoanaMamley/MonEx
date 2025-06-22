4e40c4c960dfc1620717abb6ae094d9693f2903c
Joana Mamley Teye <joanateye18@gmail.com>
2025-06-22 17:52:19 +0000

    implement frontend feature for fetching live rates and conversion

--- monex-frontend/src/app/dashboard/converter/converter.component.ts
+++ monex-frontend/src/app/dashboard/converter/converter.component.ts
@@ -1,27 +1,70 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Select } from 'primeng/select';
-import { CURRENCIES } from '../../shared/data/currencies';
-import { Currency, CurrencyData } from '../../shared/models/currency.model';
+import { Currency } from '../../shared/models/currency.model';
+import { debounceTime, distinctUntilChanged, Subject, Subscription, takeUntil } from 'rxjs';
+import { LiveDataService } from '../../shared/services/live-data.service';
+import { CommonModule } from '@angular/common';
+
+export interface ConversionData {
+  source: Currency | null;
+  target: Currency | null;
+  amount: number | null;
+}
 
 @Component({
   selector: 'converter',
   standalone: true,
-  imports: [ReactiveFormsModule, Select],
+  imports: [ReactiveFormsModule, Select, CommonModule],
   templateUrl: './converter.component.html',
   styleUrl: './converter.component.scss'
 })
-export class ConverterComponent implements OnInit{
+export class ConverterComponent implements OnInit, OnDestroy{
   @Input({required: true}) currencies!: Currency[];
-  formGroup: FormGroup | undefined;
+  result: number = 0.00
+  subscriptions: Subscription[] = [];
+  private destroy$ = new Subject<void>();
+  targetCode: string = "USD";
+  conversionForm?: FormGroup;
+
+  constructor(private liveDataService: LiveDataService){}
 
 
    ngOnInit(): void {
-      this.formGroup = new FormGroup({
-              selectedCity: new FormControl<CurrencyData | null>(null)
+      this.conversionForm = new FormGroup({
+        source: new FormControl<Currency | null>(null),
+        target: new FormControl<Currency | null>(null),
+        amount: new FormControl<number | null>(null)
       });
+
+      this.conversionForm.valueChanges.pipe(
+        debounceTime(500),
+        distinctUntilChanged(),
+        takeUntil(this.destroy$)
+      )
+      .subscribe((value: ConversionData) => {
+        const source: string | undefined = value.source?.currencyCode
+        const target: string | undefined = value.target?.currencyCode;
+        const amount: number | null = value.amount;
+        if (source && target && amount) {
+          this.liveDataService.getLiveCurrencyRateData(source, target).pipe(takeUntil(this.destroy$))
+          .subscribe(exchangeData => {
+            this.convertAmout(exchangeData, source, target, amount)
+          })
+        }
+      })
+
     }
 
-  
+    convertAmout(exchangeRates: Record<string, number>, source: string, target: string, amount: number) {
+      if (source !== "USD" && target !== "USD") {
+        this.result = exchangeRates["USD"+target] / exchangeRates["USD"+source] * amount;
+        this.targetCode = target;
+      }
+    }
 
+    ngOnDestroy(): void {
+      this.destroy$.next();
+      this.destroy$.complete();
+    }
 }
---
+++ monex-frontend/src/app/shared/services/live-data.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LiveDataService {
+
+  constructor(private http: HttpClient) { }
+
+  getLiveCurrencyRateData(source: string, target: string):Observable<Record<string, number>> {
+    return this.http.get<Record<string, number>>(`${environment.apiUrl}/live?source=${source}&target=${target}`);
+  }
+}
